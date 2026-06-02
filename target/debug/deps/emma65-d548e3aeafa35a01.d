166cc4b837d36b674713fc8ce1e944a756f326fd
Carl Harris <ceharris414@gmail.com>
2026-06-02 17:57:06 -0400

    remove stuff that shouldn't have been pushed

--- target/debug/deps/emma65-d548e3aeafa35a01.d
+++
@@ -1,6 +0,0 @@
-/home/ceharris/git/rust/emma65/target/debug/deps/emma65-d548e3aeafa35a01.d: src/main.rs src/wdc6502.rs
-
-/home/ceharris/git/rust/emma65/target/debug/deps/libemma65-d548e3aeafa35a01.rmeta: src/main.rs src/wdc6502.rs
-
-src/main.rs:
-src/wdc6502.rs:
--- target/debug/deps/emma65-efcda88a7b87c415.d
+++
@@ -1,6 +0,0 @@
-/home/ceharris/git/rust/emma65/target/debug/deps/emma65-efcda88a7b87c415.d: src/main.rs src/wdc6502.rs
-
-/home/ceharris/git/rust/emma65/target/debug/deps/emma65-efcda88a7b87c415: src/main.rs src/wdc6502.rs
-
-src/main.rs:
-src/wdc6502.rs:
