774fd748b6991f72a29995786c58129f2d262b1d
Carl Harris <ceharris414@gmail.com>
2026-06-08 05:35:29 -0400

    add doc comments

--- src/watch/error.rs
+++ src/watch/error.rs
@@ -1,4 +1,4 @@
-
+/// An error produced while scanning, parsing, or compiling a watch expression.
 #[derive(Debug, Clone, PartialEq)]
 pub struct Error {
     line: usize,
@@ -7,14 +7,28 @@ pub struct Error {
 }
 
 impl Error {
+
+    /// Creates an error instance.
+    /// # Arguments
+    /// * `line` - source line number where the error occurred
+    /// * `column` - source column number where the error occurred
+    /// * `message` - a message that describes the error that occurred
+    ///
     pub fn from(line: usize, column: usize, message: &str) -> Self {
         Self {
             line, column, message: String::from(message),
         }
     }
 
-    pub fn to_string(&self) -> String {
-        format!("at {line},{column}: {message}", line=self.line, column=self.column, message=self.message)
+}
+
+impl std::fmt::Display for Error {
+
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "at {line},{column}: {message}",
+               line=self.line, column=self.column, message=self.message)
     }
 
 }
+
+impl std::error::Error for Error {}
