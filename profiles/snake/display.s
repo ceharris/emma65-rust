2c824337c337a4de67df73dcff65fdc848fcd9ba
Carl Harris <ceharris414@gmail.com>
2026-08-14 17:04:04 -0400

    replace grid to screen coordinate LUT

--- profiles/snake/display.s
+++ profiles/snake/display.s
@@ -489,45 +489,71 @@ ui_put_food_cell:
 ;	X = grid X coordinate
 ;	Y = grid Y coordinate
 ;
+.global ui_put_grid_cup
 ui_put_grid_cup:
-				phx
-				phy
 				UI_PUT_ANSI_CSI
 				
-				; Y = 2*Y
-				tya
-				asl
-				tay
-
-				; put first digit of row
-				lda grid_y_to_row,y
-				sta CONSOLE_IO
-
-				; put second digit of row
-				lda grid_y_to_row+1,y
-				beq @put_sep		; go if only one digit
-				sta CONSOLE_IO
+				; row number is first
+				tya						; Y coordinate to A
+				inc						; rows start at 1 not 0
+				jsr _bin8bcd			; convert to BCD in W
+				lda W+1
+				beq @y_lower			; not three digits, skip MSB
+				jsr @pbcd8				; put MSB digits
+				lda W
+				jsr @pbcd8u				; put LSB digits
+				bra @put_sep
+@y_lower:
+				lda W
+				jsr @pbcd8				; put LSB digits
 @put_sep:
+				; put separator
 				lda #';'
 				sta CONSOLE_IO
 
-				; X = 2*X
-				txa
-				asl
-				tax
-
-				; put first digit of column
-				lda grid_x_to_column,x
-				sta CONSOLE_IO
-
-				; put second digit of column
-				lda grid_x_to_column+1,x
-				beq @put_cup		; go if only one digit
-				sta CONSOLE_IO
+				; column number is second
+				txa						; X coordinate to A
+				asl						; two screen columns per grid column
+				inc						; columns start at 1 not 0
+				jsr _bin8bcd			; convert to BCD in W
+				lda W+1
+				beq @x_lower			; not three digits, skip MSB
+				jsr @pbcd8				; put MSB digits
+				lda W
+				jsr @pbcd8u				; put LSB digits
+				bra @put_cup
+@x_lower:
+				lda W	
+				jsr @pbcd8				; put LSB digits					
 @put_cup:
 				UI_PUT_ANSI_CUP
-				ply
-				plx
+				rts
+
+@pbcd8:
+				pha
+				and #$f0				; is upper nibble non-zero?
+				bne @pbcd_upper			; go print it
+				bra @pbcd_lower
+				; this entry point prints a leading zero
+@pbcd8u:
+				pha
+@pbcd_upper:
+				; move upper nibble to lower nibble
+				lsr
+				lsr
+				lsr
+				lsr
+				; convert to ASCII digit
+				clc
+				adc #'0'
+				sta CONSOLE_IO
+@pbcd_lower:
+				pla						; recover arg to print
+				and #$0f				; discard upper nibble
+				; convert to ASCII digit
+				clc
+				adc #'0'
+				sta CONSOLE_IO
 				rts
 
 
@@ -707,6 +733,43 @@ show_positions:
 
 
 ;-----------------------------------------------------------------------
+; _bin8bcd:
+; Convert an 8-bit binary value in A to a two- or three-digit BCD
+; value.
+;
+; On entry:
+;	A = 8 bit value to convert
+;
+; On return:
+;	W = BCD digits that correspond to A
+;	B clobbered
+;
+_bin8bcd:
+				phx
+
+				; save value to convert
+				sta B				
+			
+				; zero out the result buffer
+				stz W				
+				stz W+1
+
+				ldx #8					; bit counter
+				sed
+@loop:
+				asl B				; high order bit of arg to carry
+				lda W
+				adc W				; double LSB + carry
+				sta W
+				lda W+1
+				adc W+1			; double MSB + carry
+				sta W+1				
+				dex
+				bne @loop
+				
+				cld
+				plx
+				rts
 
 
 ;-----------------------------------------------------------------------
@@ -736,6 +799,7 @@ _pbcd16:
 				jsr _pbcd8		; print LSB
 				rts
 
+
 ;-----------------------------------------------------------------------
 ; _pbcd8:
 ; Prints an 8-bit BCD value.
@@ -748,10 +812,10 @@ _pbcd16:
 ;
 _pbcd8:
 				pha
-				and #$f0		; is upper nibble zero?
-				bne _pbcd_upper		; go print it
-				lda #SPC		; print space instead
-				sta CONSOLE_IO		;   of leading zero
+				and #$f0				; is upper nibble non-zero?
+				bne _pbcd_upper			; go print it
+				lda #SPC				; print space instead
+				sta CONSOLE_IO			;   of leading zero
 				bra _pbcd_lower
 				; this entry point prints a leading zero
 _pbcd8u:
@@ -767,8 +831,8 @@ _pbcd_upper:
 				adc #'0'
 				sta CONSOLE_IO
 _pbcd_lower:
-				pla			; recover arg to print
-				and #$0f		; discard upper nibble
+				pla						; recover arg to print
+				and #$0f				; discard upper nibble
 				; convert to ASCII digit
 				clc
 				adc #'0'
@@ -837,103 +901,6 @@ _phex4:
 
 
 				.segment "RODATA"
-
-;-----------------------------------------------------------------------
-; grid_x_to_column:
-; This table is used to lookup the ANSI terminal column number that
-; corresponds to the X component of a grid coordinate pair. ANSI column
-; numbers start a 1 and are represented using ASCII digits. 
-;
-; Every table entry contains two bytes. Column numbers less than 10 are
-; represented as a single ASCII digit followed by ASCII NUL. The NUL 
-; character must not be transmitted to the terminal when sending such
-; column numbers.
-; 
-; The game grid consists of 40 x 23 cells, therefore there are 40 table
-; entries. Each table entry represents two consecutive columns of the
-; 80x24 terminal display.
-;
-grid_x_to_column:
-				.byte "1",0
-				.byte "3",0
-				.byte "5",0
-				.byte "7",0
-				.byte "9",0
-				.byte "11"
-				.byte "13"
-				.byte "15"
-				.byte "17"
-				.byte "19"
-				.byte "21"
-				.byte "23"
-				.byte "25"
-				.byte "27"
-				.byte "29"
-				.byte "31"
-				.byte "33"
-				.byte "35"
-				.byte "37"
-				.byte "39"
-				.byte "41"
-				.byte "43"
-				.byte "45"
-				.byte "47"
-				.byte "49"
-				.byte "51"
-				.byte "53"
-				.byte "55"
-				.byte "57"
-				.byte "59"
-				.byte "61"
-				.byte "63"
-				.byte "65"
-				.byte "67"
-				.byte "69"
-				.byte "71"
-				.byte "73"
-				.byte "75"
-				.byte "77"
-				.byte "79"
-
-;-----------------------------------------------------------------------
-; grid_y_to_row:
-; This table is used to lookup the ANSI terminal row number that
-; corresponds to the Y component of a grid coordinate pair. ANSI column
-; numbers start a 1 and are represented using ASCII digits. 
-;
-; Every table entry contains two bytes. Row numbers less than 10 are
-; represented as a single ASCII digit followed by ASCII NUL. The NUL 
-; character must not be transmitted to the terminal when sending such
-; row numbers.
-;
-; The game grid consists of 40 x 23 cells, therefore there are 23 table
-; entries. Each table entry represents one row of the 80x24 terminal
-; display.
-;
-grid_y_to_row:
-				.byte "1",0
-				.byte "2",0
-				.byte "3",0
-				.byte "4",0
-				.byte "5",0
-				.byte "6",0
-				.byte "7",0
-				.byte "8",0
-				.byte "9",0
-				.byte "10"
-				.byte "11"
-				.byte "12"
-				.byte "13"
-				.byte "14"
-				.byte "15"
-				.byte "16"
-				.byte "17"
-				.byte "18"
-				.byte "19"
-				.byte "20"
-				.byte "21"
-				.byte "22"
-				.byte "23"
 
 				.macro SGR_RESET
 				.byte ESC,"[0m"
