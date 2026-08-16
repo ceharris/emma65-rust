ad7887fc7a00b568d0acdb521ad0bcdb0df7cda1
Carl Harris <ceharris414@gmail.com>
2026-08-16 07:36:57 -0400

    remove profile source code

--- profiles/snake/prng.h.s
+++
@@ -1,9 +0,0 @@
-        .ifndef PRNG_H
-                PRNG_H = 1
-
-                .global rnd_zero
-                .global rnd_seed
-                .global rnd_next
-                .global rnd_range
-
-        .endif
